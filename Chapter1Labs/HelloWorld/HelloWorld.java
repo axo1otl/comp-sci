d835dc7c01e57a49602ea3416f78e23a15527bdf
axo1otl <mattsekiguchi@gmail.com>
2022-09-01 19:25:45 -0400

    editin

--- Chapter1Labs/HelloWorld/HelloWorld.java
+++ Chapter1Labs/HelloWorld/HelloWorld.java
@@ -1,10 +1,13 @@
+package HelloWorld;
+
 // Matheus Sekiguchi
 // August 31, 2022
 // Objective: Break down syntax of a simple program
 
 public class HelloWorld {    //Class header, curly brace opens the class
     public static void main(String[] args) {    // The method header. void means "return nothing", its a return type. main allows the program to run. Inside the parentheses are the paramaters, String is a class, [] is an array, args is the name of the parameters, this argument says "An array of strings called args"
-        System.out.print("Hello, World!");    // System is a class. A period is called a dot-operator. out is an object, it is accessing the print method. print is a method that writes to the console. The semicolon tells the compiler where the line ends.
+        System.out.println("Hello, World!");    // System is a class. A period is called a dot-operator. out is an object, it is accessing the print method. print is a method that writes to the console. The semicolon tells the compiler where the line ends.
+        System.out.println("My school laptop works!");
     } // Closes method
 }    //Closes the class
 
