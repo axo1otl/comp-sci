a1cc5cce6320cd765b60f0ec1b40ca1eadbbc4f0
axo1otl <mattsekiguchi@gmail.com>
2022-10-28 07:45:48 -0400

    code cat

--- ForLoops/Testing.java
+++ ForLoops/Testing.java
@@ -1,7 +1,15 @@
 public class Testing{
     public static void main(String[] args){
-        for (int i = 8; i > 1; i -= 2){
-            System.out.println(i + 1);
-        }
+        boolean a = true, b = true;
+        System.out.println(a||b&&!a);
     }
 }
+
+/*
+
+  ／|、
+（ﾟ､ ｡７
+ |、ﾞ~ヽ
+ じしf_, )ノ
+
+*/
