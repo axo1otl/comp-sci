37f8264ffb72ec99d4611b686e9ec3a7948602cb
axo1otl <mattsekiguchi@gmail.com>
2023-02-14 07:37:49 -0500

    2048

--- Another 2048/Game.java
+++ Another 2048/Game.java
@@ -5,11 +5,29 @@ public class Game{
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int[][] board = new int[4][4];
+        func func = new func();
+
+        // board set up
+        func.println("Enter board size!");
+        func.print("x: ");
+        int inx = scan.nextInt();
+        func.print("y:");
+        int iny = scan.nextInt();
+
+        int[][] board = new int[inx][iny];
         boolean playing = true;
         boolean running = true;
         String move = "";
-        func func = new func();
+        int sizeX = board.length;
+        int sizeY = board[0].length;
+        int newBlocks = (int)(Math.sqrt(sizeX * sizeY) / 3);
+        int itr = sizeX;
+        String ERROR = "";
+        int cnt = 0;
+
+        if (sizeY > sizeX){
+            itr = sizeY;
+        }
 
         // program loop
         while (running){
@@ -19,40 +37,89 @@ public class Game{
                     board[i][k] = 0;
                 }
             }
+
+            ERROR = ERROR + "\tboard initialized\n";
             
             // start game
-            board[func.random(0,3)][func.random(0,3)] = func.random(1,2) * 2;
-            board[func.random(0,3)][func.random(0,3)] = func.random(1,2) * 2;
+            board[func.random(0,sizeX - 1)][func.random(0,sizeY - 1)] = func.random(1,2) * 2;
+            board[func.random(0,sizeX - 1)][func.random(0,sizeY - 1)] = func.random(1,2) * 2;
 
-            /* debugging
-            for (int i = 0; i < 50; i++){
-                System.out.println(func.random(1,2));
-            }
-            */
+            ERROR = ERROR + "\ttiles placed\n";
             
             // gameplay loop
             while (playing){
                 func.printBoard(board);
-
+                
                 System.out.println("\n\nQ - quit");
                 System.out.print("W/A/S/D: "); // get the next move
                 move = scan.next();
-                
                 move = move.toUpperCase();
+
+                ERROR = ERROR + "\tmove made: " + move + "\n";
                 
                 if (move.equals("Q")){
+                    ERROR = ERROR + "\tgame quit\n";
                     playing = false;
                     running = false;
+                    break;
                 }
-
+                
+                
                 // fall blocks
-                for(int i = 0; i < 3; i++){
+                for (int i = 0; i < itr; i++){
                     func.fallBlocks(board,move);
+                    cnt++;
                 }
-                // merge blocks
+                ERROR = ERROR + cnt + "x" + ":\tfunc.fallBlocks() ran\n";
+                cnt = 0;
 
+                // merge blocks
+                func.merge(board,move);
+                ERROR = ERROR + "\tfunc.merge() ran\n";
+                
                 // fall blocks 
+                for (int i = 0; i < itr; i++){
+                    func.fallBlocks(board,move);
+                    cnt++;
+                }
+                ERROR = ERROR + cnt + "x" + ":\tfunc.fallBlocks() ran\n";
+                cnt = 0;
+                
+                // create new block
+                for (int i = 0; i < newBlocks; i++){
+                    func.createBlock(board);
+                    cnt++;
+                }
+                ERROR = ERROR + cnt + "x" + ":\tfunc.fallBlocks() ran\n";
+                
+                // check fail
+                playing = !func.fail(board);
+                ERROR = ERROR + "\tfunc.fail() ran: " + !playing + "\n";
+            }
+            
+            if (!move.equals("Q")){
+                func.println();
+                func.println("You lose!");
+                func.println("Play again?");
+                func.print("y/n: ");
+                move = scan.next();
+
+                ERROR = ERROR + "\tplayer quit game\n";
+                ERROR = ERROR + "\tplayer selected:" + move + "\n";
+            }
+            else {
+                func.println();
+                func.println("Quitting...");
+                ERROR = ERROR + "\tprogram quitting...\n";
+                break;
+            }
+            
+            if (!move.toUpperCase().equals("Y")){
+                playing = false;
+                running = false;
             }
         }
+        ERROR = ERROR + "\tprogram quit successfully\n";
+        func.println(ERROR);
     }
 }
--- Another 2048/func.java
+++ Another 2048/func.java
@@ -5,14 +5,34 @@ public class func{
         k++;
         return((int)(Math.random() * (k - (i)) + i));
     }
+    
+    public void print(){System.out.print("");}
+    public void print(int _int){System.out.print(_int);}
+    public void print(byte _byte){System.out.print(_byte);}
+    public void print(long _long){System.out.print(_long);}
+    public void print(char _char){System.out.print(_char);}
+    public void print(short _short){System.out.print(_short);}
+    public void print(float _float){System.out.print(_float);}
+    public void print(double _double){System.out.print(_double);}
+    public void print(String _string){System.out.print(_string);}
+    public void print(boolean _boolean){System.out.print(_boolean);}
+    
+    public void println(){System.out.println();}
+    public void println(int _int){System.out.println(_int);}
+    public void println(byte _byte){System.out.println(_byte);}
+    public void println(long _long){System.out.println(_long);}
+    public void println(char _char){System.out.println(_char);}
+    public void println(short _short){System.out.println(_short);}
+    public void println(float _float){System.out.println(_float);}
+    public void println(double _double){System.out.println(_double);}
+    public void println(String _string){System.out.println(_string);}
+    public void println(boolean _boolean){System.out.println(_boolean);}
 
     public void printBoard(int[][] board){
-
         for (int i = 0; i < board.length; i++){
             System.out.println("");
             System.out.println("");
             System.out.println("");
-
             for (int k = 0; k < board[0].length; k++){
                 if (board[i][k] == 0){
                     System.out.print(".\t");
@@ -25,52 +45,149 @@ public class func{
     }
 
     public void fallBlocks(int[][] board, String move){
-
         if (move.equals("W")){
             for (int i = 1; i < board.length; i++){        // down
                 for (int k = 0; k < board[0].length; k++){ // across
-
                     if (board[i-1][k] == 0){
                         board[i-1][k] = board[i][k];
                         board[i][k] = 0;
                     }
-
-
                 }
             }
-        }
-
+            return;
+        }       
         if (move.equals("S")){
             for (int i = board.length - 2; i >= 0; i--){   // down
-                for (int k = 0; k < board[0].length; k++){ // across
-                    System.out.println(board[i][k]);
-
+                for (int k = 0; k < board[0].length; k++){ // across                   
                     if (board[i+1][k] == 0){
                         board[i+1][k] = board[i][k];
                         board[i][k] = 0;
                     }
-
-
                 }
             }
+            return;
         }
+        if (move.equals("A")){
+            for (int i = 0; i < board.length; i++){        // down
+                for (int k = 1; k < board[0].length; k++){ // across
+                    if (board[i][k-1] == 0){
+                        board[i][k-1] = board[i][k];
+                        board[i][k] = 0;
+                    }
+                }
+            }
+            return;
+        }
+        if (move.equals("D")){
+            for (int i = 0; i < board.length; i++){   // down
+                for (int k = board[0].length - 2; k >= 0; k--){ // across
+                    if (board[i][k+1] == 0){
+                        board[i][k+1] = board[i][k];
+                        board[i][k] = 0;
+                    }
+                }
+            }
+            return;
+        }
+    }
 
+    public void merge(int[][] board, String move){
+        if (move.equals("W")){
+            for (int i = 1; i < board.length; i++){        // down
+                for (int k = 0; k < board[0].length; k++){ // across
+                    if (board[i-1][k] == board[i][k]){
+                        board[i-1][k] = board[i][k] * 2;
+                        board[i][k] = 0;
+                    }
+                }
+            }
+            return;
+        }
         if (move.equals("S")){
             for (int i = board.length - 2; i >= 0; i--){   // down
                 for (int k = 0; k < board[0].length; k++){ // across
-                    System.out.println(board[i][k]);
-
-                    if (board[i+1][k] == 0){
-                        board[i+1][k] = board[i][k];
+                    if (board[i+1][k] == board[i][k]){
+                        board[i+1][k] = board[i][k] * 2;
                         board[i][k] = 0;
                     }
+                }
+            }
+            return;
+        }
+        if (move.equals("A")){
+            for (int i = 0; i < board.length; i++){        // down
+                for (int k = 1; k < board[0].length; k++){ // across
+                    if (board[i][k-1] == board[i][k]){
+                        board[i][k-1] = board[i][k] * 2;
+                        board[i][k] = 0;
+                    }
+                }
+            }
+            return;
+        }
+        if (move.equals("D")){
+            for (int i = 0; i < board.length; i++){   // down
+                for (int k = board[0].length - 2; k >= 0; k--){ // across
+                    if (board[i][k+1] == board[i][k]){
+                        board[i][k+1] = board[i][k] * 2;
+                        board[i][k] = 0;
+                    }
+                }
+            }
+            return;
+        }
+    }
 
+    public void createBlock(int[][] board){
+        int x = random(0,board.length - 1);
+        int y = random(0,board[0].length - 1);
+        int val = random(1,2) * 2;
+        boolean valid = false;
+        
+        while(!valid){
+            if(board[x][y] == 0){
+                board[x][y] = val;
+                valid = true;
+                //println("Valid");
+            }
+            else{
+                x = random(0,3);
+                y = random(0,3);
+                //println("Invalid");
+            }
+        }
+    }
 
+    public boolean fail(int[][] board){
+        for (int i = 1; i < board.length; i++){        // down
+            for (int k = 0; k < board[0].length; k++){ // across
+                if (board[i-1][k] == board[i][k]){
+                    return(false);
                 }
             }
         }
-
-
+        for (int i = board.length - 2; i >= 0; i--){   // down
+            for (int k = 0; k < board[0].length; k++){ // across
+                if (board[i+1][k] == board[i][k]){
+                    return(false);
+                }
+            }
+        }
+        for (int i = 0; i < board.length; i++){        // down
+            for (int k = 1; k < board[0].length; k++){ // across
+                if (board[i][k-1] == board[i][k]){
+                    return(false);
+                }
+            }
+        }
+        for (int i = 0; i < board[0].length; i++){   // down
+            for (int k = board.length - 2; k >= 0; k--){ // across
+                if (board[i][k+1] == board[i][k]){
+                    return(false);
+                }
+            }
+        }
+        return(true);
     }
 
     //
